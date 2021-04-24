7f3bafd9d0562de7e714b574dc6017c319452c1d
Sandy K <skuan8@my.bcit.ca>
2021-04-24 12:54:15 -0700

    add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.scss"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
@@ -6,6 +7,7 @@ import LoadingOne from "./components/Loading/Loading-1"
 import LoadingTwo from "./components/Loading/Loading-2"
 import LoadingThree from "./components/Loading/Loading-3"
 import LoadingFour from "./components/Loading/Loading-4"
+import SigninForm from "./components/SigninForm/SigninForm"
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           <Route path="/loading2" component={LoadingTwo} />
           <Route path="/loading3" component={LoadingThree} />
           <Route path="/loading4" component={LoadingFour} />
+          <Route path="/signin" component={SigninForm} />
         </div>
       </Switch>
     </BrowserRouter>
